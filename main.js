6d135118aca8d4aa5be21967f64b1f48377955b8
Luka Martin Leguizamon <lukaleguizamon123@gmail.com>
2024-01-28 17:47:31 -0300

    agregando filter

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function verAlumnos(){
     class Estudiante {
-    constructor(nombre, edad, calificaciones) {
+    constructor (nombre, edad, calificaciones) {
     this.nombre = nombre;
     this.edad = edad;
     this.calificaciones = calificaciones;
     }
 
     obtenerPromedio() {
-        const sumaCalificaciones = this.calificaciones.reduce((suma, calificacion) => suma + calificacion, 0);
-        const promedio = sumaCalificaciones / this.calificaciones.length;
+        let sumaCalificaciones = this.calificaciones.reduce((suma, calificacion) => suma + calificacion, 0);
+        let promedio = sumaCalificaciones / this.calificaciones.length;
         return parseFloat(promedio.toFixed(2)); // redondeo los decimales con toFixed
 
         }
@@ -16,7 +16,7 @@ function verAlumnos(){
 
     //Lista de estudiantes
     
-        const estudiantes = [
+        let estudiantes = [
             new Estudiante('Pedro', 20, [4, 6, 8]),
             new Estudiante('María', 22, [5, 4, 7]),
             new Estudiante('Carlos', 31, [3, 2, 7]),
@@ -27,16 +27,16 @@ function verAlumnos(){
 
 
     // Mostrar la lista de nombres de estudiantes
-        const nombresDeEstudiantes = estudiantes.map(estudiante => estudiante.nombre).join(', ');
+        let nombresDeEstudiantes = estudiantes.map(estudiante => estudiante.nombre).join(', ');
         alert(`Nombres de estudiantes: ${nombresDeEstudiantes}`);
 
 
     // Solicitar un nombre de estudiante
-        const nombreBuscado = prompt('Ingrese el nombre del estudiante a buscar:');
-        const estudianteEncontradoPorNombre = estudiantes.find(estudiante => estudiante.nombre === nombreBuscado);
+        let nombreBuscado = prompt('Ingrese el nombre del estudiante a buscar:');
+        let estudianteEncontradoPorNombre = estudiantes.find(estudiante => estudiante.nombre === nombreBuscado);
 
     // Verificar si el nombre esta en la lista
-        const nombreEncontrado = nombresDeEstudiantes.includes(nombreBuscado);
+        let nombreEncontrado = nombresDeEstudiantes.includes(nombreBuscado);
         alert(`¿El nombre "${nombreBuscado}" está en la lista de estudiantes? ${nombreEncontrado ? 'Sí' : 'No'}`);
 
     // Mostrar el estudiante encontrado o un mensaje si no se encuentra
@@ -56,13 +56,13 @@ function verAlumnos(){
         }
 
         //Solicitar promedio para filtrar
-        const promedioMinimo = parseFloat(prompt('Ingrese el promedio mínimo para filtrar los estudiantes:'));
-        const estudiantesFiltrados = filtrarEstudiantesPorPromedio(promedioMinimo);
+        let promedioMinimo = parseFloat(prompt('Ingrese el promedio mínimo para filtrar los estudiantes:'));
+        let estudiantesFiltrados = filtrarEstudiantesPorPromedio(promedioMinimo);
         
         // Mostrar el promedio o un mensaje si no se encuentra
 
         if (estudiantesFiltrados.length > 0) {
-            const nombresDeEstudiantesFiltrados = estudiantesFiltrados.map(estudiante => estudiante.nombre).join(', ');
+            let nombresDeEstudiantesFiltrados = estudiantesFiltrados.map(estudiante => estudiante.nombre).join(', ');
             alert(`Estudiantes con promedio superior a ${promedioMinimo}:\n${nombresDeEstudiantesFiltrados}`);
         } else {
             alert(`No se encontraron estudiantes con un promedio superior a ${promedioMinimo}.`);
